77497be41e2032984066aa5d613fcdd4f0be62e9
Jessibel <33192144+Jessibel5jzt@users.noreply.github.com>
2020-01-02 16:31:05 +0800

    Add files via upload

---
+++ PrefabApply.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace FFTAI
+{
+    public class PrefabApply
+    {
+        [InitializeOnLoadMethod]
+        static void StartInitializeOnLoadMethod()
+        {
+            PrefabUtility.prefabInstanceUpdated = delegate (GameObject instance)
+            {
+                Debug.LogError("待添加的PrefabApply事件");
+            };
+        }
+    }
+}
