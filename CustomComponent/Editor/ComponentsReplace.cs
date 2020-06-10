82f54a5554025cc6d5a968c159f7cf1076968468
Jessibel5jzt <jessibel_jzt@163.com>
2020-06-10 10:22:04 +0800

    fix

--- CustomComponent/Editor/ComponentsReplace.cs
+++ CustomComponent/Editor/ComponentsReplace.cs
@@ -4,6 +4,7 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using System;
 
 	public class ComponentsReplace : EditorWindow
 	{
@@ -17,92 +18,34 @@ using System.Collections.Generic;
 		{
 			if (GUILayout.Button("Text替换为TextPro"))
 			{
-				TextToPro();
-			}
-
-		}
-		public static Text ChangeT(GameObject tGo)
-		{
-			Text t = tGo.GetComponent<Text>();
-			Undo.RecordObject(tGo, tGo.name);
-			DestroyImmediate(tGo.GetComponent<Text>());
-			var tp = tGo.AddComponent<TextPro>();
-			tp.text = t.text;
-			tp.font = t.font;
-			tp.fontStyle = t.fontStyle;
-			tp.fontSize = t.fontSize;
-			tp.lineSpacing = t.lineSpacing;
-			tp.supportRichText = t.supportRichText;
-			tp.alignment = t.alignment;
-			tp.alignByGeometry = t.alignByGeometry;
-			tp.horizontalOverflow = t.horizontalOverflow;
-			tp.verticalOverflow = t.verticalOverflow;
-			tp.resizeTextForBestFit = t.resizeTextForBestFit;
-			tp.resizeTextMinSize = t.resizeTextMinSize;
-			tp.resizeTextMaxSize = t.resizeTextMaxSize;
-			tp.color = t.color;
-			tp.material = t.material.name.CompareTo("Default UI Material") == 0 ? null : t.material;
-			tp.raycastTarget = t.raycastTarget;
-			tp.TestNewProperty = "测试用自定义新属性";
-			EditorUtility.SetDirty(tGo);
-			return tp;
-		}
-
-
-
-		private static int _replaceCount = 0;
-		public static Text ChangeTextReferenceRelationship(Text item)
-		{
-			if (item != null && item.GetType() != typeof(TextPro))
-			{
-				_replaceCount += 1;
-				var timetextGo = item.gameObject;
-				return ChangeT(timetextGo);
-			}
-			return null;
-		}
-		public static void TextToPro()
-		{
-			var gpList = GetAllObjsOfType<GamePanel>();
-			foreach (var item in gpList)
-			{
-				item.Timetext = ChangeTextReferenceRelationship(item.Timetext);
-				item.Gradetext = ChangeTextReferenceRelationship(item.Gradetext);
-			}
-
-			var admList = GetAllObjsOfType<AudioMgr>();
-			foreach (var item in admList)
-			{
-				item.musicTypetxt = ChangeTextReferenceRelationship(item.musicTypetxt);
-				item.musicNametxt = ChangeTextReferenceRelationship(item.musicNametxt);
-			}
-
-			var ddList = GetAllObjsOfType<Dropdown>();
-			foreach (var item in ddList)
-			{
-				item.captionText = ChangeTextReferenceRelationship(item.captionText);
-				item.itemText = ChangeTextReferenceRelationship(item.itemText);
-			}
-
-			var ipfList = GetAllObjsOfType<InputField>();
-			foreach (var item in ipfList)
-			{
-				item.textComponent = ChangeTextReferenceRelationship(item.textComponent);
-				item.placeholder = ChangeTextReferenceRelationship(item.placeholder as Text);
-			}
-
-			var tList = GetAllObjsOfType<Text>();
-			for (int i = 0; i < tList.Count; i++)
-			{
-				if (tList[i].GetType() != typeof(TextPro))
+				if (EditorUtility.DisplayDialog("确认替换", "是否确认替换场景内所有Text组件为TextPro？", "确定", "取消"))
 				{
-					tList[i] = ChangeT(tList[i].gameObject);
+					TextToPro();
 				}
 			}
 
-			Debug.Log($"替换成功，共替换{_replaceCount}处。");
-			_replaceCount = 0;
+			if (GUILayout.Button("TextPro替换为Text"))
+			{
+				if (EditorUtility.DisplayDialog("确认替换", "此举可能导致TextPro组件的新增属性丢失，若未使用新增属性可完整替换，是否确认替换？", "确定", "取消"))
+				{
+					ProToText();
+				}
+			}
+
 		}
+
+
+
+		/// <summary>
+		/// 计数
+		/// </summary>
+		private static int _replaceCount = 0;
+		/// <summary>
+		/// 查找组件
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="onlyRoot">是否仅父节点</param>
+		/// <returns></returns>
 		public static List<T> GetAllObjsOfType<T>(bool onlyRoot = false) where T : Component
 		{
 			T[] Objs = (T[])Resources.FindObjectsOfTypeAll(typeof(T));
@@ -139,5 +82,141 @@ using System.Collections.Generic;
 
 			return returnObjs;
 		}
+
+
+		#region TextToPro
+		public static void TextToPro()
+		{
+			_replaceCount = 0;
+
+			var ddList = GetAllObjsOfType<Dropdown>();
+			foreach (var item in ddList)
+			{
+				item.captionText = ChangeTextReferenceRelationship(item.captionText);
+				item.itemText = ChangeTextReferenceRelationship(item.itemText);
+			}
+
+			var ipfList = GetAllObjsOfType<InputField>();
+			foreach (var item in ipfList)
+			{
+				item.textComponent = ChangeTextReferenceRelationship(item.textComponent);
+				item.placeholder = ChangeTextReferenceRelationship(item.placeholder as Text);
+			}
+
+			var tList = GetAllObjsOfType<Text>();
+			for (int i = 0; i < tList.Count; i++)
+			{
+				if (tList[i].GetType() != typeof(TextPro))
+				{
+					ChangeT(tList[i].gameObject);
+				}
+			}
+
+			Debug.Log($"Text已全部替换为TextPro，共替换{_replaceCount}处。");
+		}
+		public static TextPro ChangeTextReferenceRelationship(Text item)
+		{
+			if (item == null) return null;
+			if (item.GetType() == typeof(TextPro)) return item as TextPro;
+
+			var timetextGo = item.gameObject;
+			return ChangeT(timetextGo);
+		}
+		public static TextPro ChangeT(GameObject tGo)
+		{
+			var t = tGo.GetComponent<Text>();
+			var enabled = t.enabled;
+			Undo.RecordObject(tGo, tGo.name);
+			DestroyImmediate(tGo.GetComponent<Text>());
+			var tp = tGo.AddComponent<TextPro>();
+			tp.text = t.text;
+			tp.font = t.font;
+			tp.fontStyle = t.fontStyle;
+			tp.fontSize = t.fontSize;
+			tp.lineSpacing = t.lineSpacing;
+			tp.supportRichText = t.supportRichText;
+			tp.alignment = t.alignment;
+			tp.alignByGeometry = t.alignByGeometry;
+			tp.horizontalOverflow = t.horizontalOverflow;
+			tp.verticalOverflow = t.verticalOverflow;
+			tp.resizeTextForBestFit = t.resizeTextForBestFit;
+			tp.resizeTextMinSize = t.resizeTextMinSize;
+			tp.resizeTextMaxSize = t.resizeTextMaxSize;
+			tp.color = t.color;
+			tp.material = t.material.name.CompareTo("Default UI Material") == 0 ? null : t.material;
+			tp.raycastTarget = t.raycastTarget;
+			tp.TestNewProperty = "测试用自定义新属性";
+			tp.enabled = enabled;
+			EditorUtility.SetDirty(tGo);
+			_replaceCount += 1;
+			return tp;
+		}
+		#endregion
+
+		#region ProToText
+		private void ProToText()
+		{
+			_replaceCount = 0;
+
+			var ddList = GetAllObjsOfType<Dropdown>();
+			foreach (var item in ddList)
+			{
+				item.captionText = ChangeTextProReferenceRelationship(item.captionText);
+				item.itemText = ChangeTextProReferenceRelationship(item.itemText);
+			}
+
+			var ipfList = GetAllObjsOfType<InputField>();
+			foreach (var item in ipfList)
+			{
+				item.textComponent = ChangeTextProReferenceRelationship(item.textComponent);
+				item.placeholder = ChangeTextProReferenceRelationship(item.placeholder as Text);
+			}
+
+			var tList = GetAllObjsOfType<TextPro>();
+			for (int i = 0; i < tList.Count; i++)
+			{
+				ChangeTP(tList[i].gameObject);
+			}
+
+			Debug.Log($"TextPro已全部替换为Text，共替换{_replaceCount}处。");
+		}
+		public static Text ChangeTextProReferenceRelationship(Text item)
+		{
+			if (item != null)
+			{
+				var timetextGo = item.gameObject;
+				return ChangeTP(timetextGo);
+			}
+			return null;
+		}
+		public static Text ChangeTP(GameObject tGo)
+		{
+			var tp = tGo.GetComponent<Text>();
+			var enabled = tp.enabled;
+			Undo.RecordObject(tGo, tGo.name);
+			DestroyImmediate(tGo.GetComponent<Text>());
+			var t = tGo.AddComponent<Text>();
+			t.text = tp.text;
+			t.font = tp.font;
+			t.fontStyle = tp.fontStyle;
+			t.fontSize = tp.fontSize;
+			t.lineSpacing = tp.lineSpacing;
+			t.supportRichText = tp.supportRichText;
+			t.alignment = tp.alignment;
+			t.alignByGeometry = tp.alignByGeometry;
+			t.horizontalOverflow = tp.horizontalOverflow;
+			t.verticalOverflow = tp.verticalOverflow;
+			t.resizeTextForBestFit = tp.resizeTextForBestFit;
+			t.resizeTextMinSize = tp.resizeTextMinSize;
+			t.resizeTextMaxSize = tp.resizeTextMaxSize;
+			t.color = tp.color;
+			t.material = tp.material.name.CompareTo("Default UI Material") == 0 ? null : tp.material;
+			t.raycastTarget = tp.raycastTarget;
+			t.enabled = enabled;
+			EditorUtility.SetDirty(tGo);
+			_replaceCount += 1;
+			return t;
+		}
+		#endregion
 
 	}
